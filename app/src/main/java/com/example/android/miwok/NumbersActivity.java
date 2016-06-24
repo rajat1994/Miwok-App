9c66c3cce41d466b8f99743c33300b89cae20033
rajat1994 <rajatarora25@yahoo.in>
2016-06-24 16:36:41 +0530

    ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,26 +4,39 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        String[] words = new String[10];
-        words[0]= "one";
-        words[1]= "two";
-        words[2]= "three";
-        words[3]= "four";
-        words[4]= "five";
-        words[5]= "six";
-        words[6]= "seven";
-        words[7]= "eight";
-        words[8]= "nine";
-        words[9]= "ten";
+        ArrayList<String> words = new ArrayList<String>();
 
-        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(2));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(3));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(4));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(5));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(6));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(7));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(8));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(9));
+
 
 
 
