44d53c7f10e0b289bf9611e4be4c96c86d0a7efc
rajat1994 <rajatarora25@yahoo.in>
2016-06-26 14:05:42 +0530

    adding textview dynamically

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -26,16 +27,14 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("ten");
 
 
-        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
-        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
-        Log.v("NumbersActivity", "Word at index 0: " + words.get(2));
-        Log.v("NumbersActivity", "Word at index 1: " + words.get(3));
-        Log.v("NumbersActivity", "Word at index 0: " + words.get(4));
-        Log.v("NumbersActivity", "Word at index 1: " + words.get(5));
-        Log.v("NumbersActivity", "Word at index 0: " + words.get(6));
-        Log.v("NumbersActivity", "Word at index 1: " + words.get(7));
-        Log.v("NumbersActivity", "Word at index 0: " + words.get(8));
-        Log.v("NumbersActivity", "Word at index 1: " + words.get(9));
+        LinearLayout rootview = (LinearLayout)findViewById(R.id.rootview);
+        TextView wordview = new TextView(this);
+        wordview.setText(words.get(0));
+        rootview.addView(wordview);
+
+        TextView wordview2 = new TextView(this);
+        wordview2.setText(words.get(1));
+        rootview.addView(wordview2);
 
 
 
